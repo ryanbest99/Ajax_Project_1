4ce26264fae809017d8681ee7f5ad1fc57f5ce0c
Ryan Park <ryan.developer99@gmail.com>
2021-04-09 20:27:25 +0900

    Fifth commit: Implement the API successfully in app.js file

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 const btn = document.querySelector(".btn");
 const content = document.querySelector(".content");
-const url = "http://127.0.0.1:5502/index.html";
+const URL = "https://api.chucknorris.io/jokes/random";
 
 btn.addEventListener("click", () => {
   console.log("HELLO");
+  getData(URL);
 });
+
+function getData(url) {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", url);
+  xhr.send();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== 4) return;
+    if (xhr.status === 200) {
+      console.log(xhr.responseText);
+      const data = JSON.parse(xhr.responseText);
+      console.log(data);
+      console.log(data.value);
+      content.textContent = data.value;
+    } else {
+      console.log({
+        status: xhr.status,
+        text: xhr.statusText,
+      });
+    }
+  };
+}
