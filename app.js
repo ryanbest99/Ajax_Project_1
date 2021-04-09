82fbc5df4ce1d50800c6551d5c51b1aef274d71b
Ryan Park <ryan.developer99@gmail.com>
2021-04-09 20:13:41 +0900

    Fourth commit : create btn, content, url element and click event on btn

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
+const btn = document.querySelector(".btn");
+const content = document.querySelector(".content");
 const url = "http://127.0.0.1:5502/index.html";
+
+btn.addEventListener("click", () => {
+  console.log("HELLO");
+});
